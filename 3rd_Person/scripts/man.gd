f83599c3d3946595dcaf0394135314c3cae1f573
Sandeep Jadam <withsj.in@gmail.com>
2024-07-29 23:51:52 +0530

    ledge detect and jump and climb  char

--- 3rd_Person/scripts/man.gd
+++ 3rd_Person/scripts/man.gd
@@ -4,8 +4,15 @@ extends CharacterBody3D
 @onready var visual = $visual
 @onready var animplayer = $visual/man/AnimationPlayer
 @onready var animTree = $visual/man/AnimationTree
+@onready var headRay = $visual/Rays/Head_ray
+@onready var shoulderRay = $visual/Rays/Sholder_ray
+@onready var facingRay = $visual/Rays/Facing_ray
+@onready var ledgeRay = $visual/Rays/Ledge_holder/Ledge_ray
+@onready var ledgeMarker = $visual/Rays/Ledge_holder/MeshInstance3D
+@onready var ledgeHolder = $visual/Rays/Ledge_holder
+
 var SPEED = 1.5
-const JUMP_VELOCITY = 3.5
+const JUMP_VELOCITY = 4
 var mouse_senstivety = .25
 var walking_speed = 1.5
 var running_speed = 3.5
@@ -27,12 +34,13 @@ func _input(event):
 		rotate_y(deg_to_rad(-event.relative.x * mouse_senstivety))
 		visual.rotate_y(deg_to_rad(event.relative.x * mouse_senstivety))
 		player_camera.rotate_x(deg_to_rad((-event.relative.y * mouse_senstivety)))
-		
+	
 func _physics_process(delta):
 	
-			# Add the gravity.
+	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
+		
 	#Running
 	if Input.is_action_pressed("run") and is_on_floor():
 		SPEED = running_speed
@@ -44,10 +52,8 @@ func _physics_process(delta):
 #		jump_hold = Time.get_ticks_msec()
 #		print(jump_hold)
 	
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-#		print(Time.get_ticks_msec() - jump_hold) 
-		velocity.y = JUMP_VELOCITY
-		jump = true
+	jump_and_climb()
+	ledge_detect()
 		#$CollisionShape3D.scale.y -= .500
 
 		
@@ -75,3 +81,31 @@ func _physics_process(delta):
 	#animTree.set("parameters/conditions/turn_back",Input.is_action_pressed("ui_down"))
 	
 	move_and_slide()
+
+
+func ledge_detect():
+	var hit1 = facingRay.get_collision_point()
+	var hit2 = ledgeRay.get_collision_point()
+	var offset = Vector3(0,2.5,0)
+	if facingRay.is_colliding():
+		ledgeHolder.global_transform.origin = hit1 + offset
+		ledgeMarker.global_transform.origin = hit2
+		print(ledgeMarker.position)
+		ledgeMarker.visible = true
+		ledgeRay.enabled = true
+	else:
+		ledgeMarker.visible = false
+		ledgeRay.enabled = false
+func jump_and_climb():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+#		print(Time.get_ticks_msec() - jump_hold) 
+		velocity.y = JUMP_VELOCITY
+		
+		jump = true
+	
+	if not headRay.is_colliding() and  shoulderRay.is_colliding():
+			#print("Climb")
+			velocity = Vector3.ZERO
+			gravity = 0
+	else:
+		gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
